69315f94237119afdbb34a0f3bc2da9efcee83b4
Mohammad Tahir <starghareeb@gmail.com>
2025-10-07 19:07:52 +0500

    json server package version changed ...

--- src/store/api.js
+++ src/store/api.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:3000/', 
+    baseUrl: 'http://localhost:3001/', // Updated to match JSON Server port
   }),
   tagTypes: ['Todos'], // Define tag types for caching and invalidation
   endpoints: (builder) => ({
